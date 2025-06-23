99760080de33f539eb078a6ecf110ac80844f4bb
Aurelia <aurelia@acuteaura.net>
2025-06-23 13:23:49 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,6 +9,8 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+mkdir /opt /nix
+
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
