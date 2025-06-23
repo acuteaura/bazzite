49d1cbe354fc654daca2ca02bc3d5b3a7068f479
Aurelia <aurelia@acuteaura.net>
2025-06-23 17:08:25 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,7 +7,8 @@ mkdir -p /nix
 # librewolf
 curl -fsSL https://repo.librewolf.net/librewolf.repo > /etc/yum.repos.d/librewolf.repo
 dnf -y install librewolf
-ln -s /usr/lib/mozilla/native-messaging-hosts /usr/lib/librewolf/native-messaging-hosts
+# fuck this i give up
+#ln -s /usr/lib/mozilla/native-messaging-hosts /usr/lib/librewolf/native-messaging-hosts
 
 # 1password
 # "it's just a bunch of shell scripts is the joke nix users don't get"
