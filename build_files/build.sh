bc3bd8350d046154543fb99e1d8910b2c6e9b17e
Aurelia <aurelia@acuteaura.net>
2025-06-25 08:58:08 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,7 @@ mkdir -p /nix
 
 # librewolf
 curl -fsSL https://repo.librewolf.net/librewolf.repo > /etc/yum.repos.d/librewolf.repo
-dnf -y install librewolf
+dnf -y install librewolf thunar thunar-volman thunar-archive-plugin
 
 # fuck this i give up
 #ln -s /usr/lib/mozilla/native-messaging-hosts /usr/lib/librewolf/native-messaging-hosts
